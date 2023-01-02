bb92d27989109792092ed6abc063bccc9c15ae81
iXmalMonk <ixmal999@gmail.com>
2023-01-02 22:37:03 +0300

    deleted colored print in console

--- Game_OOP/source/utilts/Console.cpp
+++ Game_OOP/source/utilts/Console.cpp
@@ -6,16 +6,6 @@ void console::hide()
 		SW_HIDE);
 }
 
-void console::print(string _text, Color _font, Color _background)
-{
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole,
-		int(_font) + (int(_background) << 4));
-	cout << _text;
-	SetConsoleTextAttribute(hConsole,
-		int(Color::GRAY));
-}
-
 void console::show()
 {
 	ShowWindow(GetConsoleWindow(),
